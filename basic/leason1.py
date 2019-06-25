e96539bc6fba441ba7b97ac9a5f911e09019e396
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-25 20:40:51 +0800

    basic operation

--- basic/leason1.py
+++ basic/leason1.py
@@ -1,8 +1,20 @@
 import time
 
+# print
 print("1")
-time.sleep(1)
-print("2")
-time.sleep(2)
 
-#test git
+time.sleep(1)
+
+# basic operation
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(1 / 1)
+
+# ^ -> **
+print(3 ** 3)
+print(4 ** 4)
+
+# %
+print(9 % 2)
+
