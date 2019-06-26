f7d333004cbed2abc4e5fb741ab298917225c751
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-26 21:28:25 +0800

    Fibonacci sequence

---
+++ basic/leason5.py
@@ -0,0 +1,23 @@
+# iterator
+
+# define Fib class
+class Fib(object):
+    def __init__(self, max):
+        self.max = max
+        self.n, self.a, self.b = 0, 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n < self.max:
+            r = self.b
+            self.a, self.b = self.b, self.a + self.b
+            self.n += 1
+            return r
+        return StopIteration()
+
+
+# using fib object
+for i in Fib(10):
+    print(i)
