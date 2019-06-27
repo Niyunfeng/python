f348be93699772520f00080235ff939a4f78753f
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-27 22:05:20 +0800

    function

---
+++ basic/leason7.py
@@ -0,0 +1,9 @@
+# def function
+def function(a, b, value=10):
+    print("This is a function")
+    c = a + b
+    print("a + b :", c)
+    print("Function default value :", value)
+
+
+function(1, 2)
