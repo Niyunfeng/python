e44ae74b485851e87947440ed665813cc7045113
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-27 21:33:21 +0800

    if else

---
+++ basic/leason6.py
@@ -0,0 +1,19 @@
+# if elif else
+
+a = 1
+b = 2
+
+if a == b:
+    print("a is equal to b")
+else:
+    print("a is unequal to b")
+
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+
+# if-else
+var1 = False
+var2 = "True" if var1 else "false"
+print(var2)
