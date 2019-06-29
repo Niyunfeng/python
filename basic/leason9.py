bbc41f6df08070358cfc35e454f0d47d778365b8
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-29 22:06:23 +0800

    Read/Write file update2

--- basic/leason9.py
+++ basic/leason9.py
@@ -4,7 +4,7 @@
 text = "This is my first line.\nThis is my second line.\nThis is my third line.\n"
 print(text)
 
-text = "\tThis is my first line.\n\tThis is my second line.\n\tThis is my third line."
+text = "\tThis is my first line.\n\tThis is my second line.\n\tThis is my third line.\n"
 print(text)
 
 my_file = open("text.txt", "w")  # 用法: open('文件名','形式'), 其中形式有'w':write;'r':read.
@@ -13,9 +13,25 @@ my_file.close()  # 关闭文件
 
 print("--------------------------")
 
+append_text = "\tThis is append line."
+my_file = open("text.txt", "a")
+my_file.write(append_text)
+my_file.close()
+
 my_file = open("text.txt")
 try:
     all_the_text = my_file.read()
     print(all_the_text)
 finally:
-    my_file.close
+    my_file.close()
+
+print("--------------------------")
+
+file = open("text.txt", "r")
+try:
+    context = file.readline()  # 读取第一行
+    print(context)
+    context = file.readlines(2)  # 读取第二行文本
+    print(context)
+finally:
+    file.close()
