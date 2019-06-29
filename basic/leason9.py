b81f85e781407f6c20ce1481fde5bfa72fefeb50
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-29 21:40:17 +0800

    Read/Write file

---
+++ basic/leason9.py
@@ -0,0 +1,12 @@
+# read/write file
+
+# \n line commend
+text = "This is my first line.\nThis is my second line.\nThis is my third line.\n"
+print(text)
+
+text = "\tThis is my first line.\n\tThis is my second line.\n\tThis is my third line."
+print(text)
+
+my_file = open("text.txt", "w")  # 用法: open('文件名','形式'), 其中形式有'w':write;'r':read.
+my_file.write(text)  # 该语句会写入先前定义好的 text
+my_file.close()  # 关闭文件
