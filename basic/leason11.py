55229a5c15238d9dd791844e7977fe3f99858da2
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-30 08:39:47 +0800

    1.Input use case. 2.Update .gitignore file

---
+++ basic/leason11.py
@@ -0,0 +1,23 @@
+# input
+
+# a_input = int(input("Please input a number: "))
+# if a_input == 1:
+#     print("This is a good one")
+# elif a_input == 2:
+#     print("See you next time")
+# else:
+#     print("Good luck")
+
+# input extension
+score = int(input("Please input your score: \n"))
+if score >= 90:
+    print("Congradulation, you get an A")
+elif score >= 80:
+    print("You get a B")
+elif score >= 70:
+    print("You get a C")
+elif score >= 60:
+    print("You get a D")
+else:
+    print("Sorry, you are failed!")
+
