0d25d8e915ef0fe826126b6c173b814da0ae6a6b
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-06-30 08:16:03 +0800

    Class __init__

--- basic/leason10.py
+++ basic/leason10.py
@@ -29,3 +29,26 @@ print(cal.add(1, 2))
 print(cal.minus(1, 2))
 print(cal.times(2, 1))
 print(cal.divide(2, 1))
+
+
+class Calaulator1:
+    name = "Good Calaulator"
+    price = 18
+
+    def __init__(self, name, price, height, wight, width):
+        self.name = name
+        self.price = price
+        self.height = height
+        self.wight = wight
+        self.width = width
+
+
+c = Calaulator1("Bad Calaulator", 10, 11, 12, 13)
+print(c.price)
+print(c.name)
+print(c.height)
+print(c.width)
+print(c.wight)
+
+c.wight = 100
+print(c.wight)
