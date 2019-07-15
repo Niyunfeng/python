b99ea421d2a82ea3381cdb8c59e8e25248345adf
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2019-07-15 21:25:31 +0800

    Build Neural Network

--- Tensorflow/leason6.py
+++ Tensorflow/leason6.py
@@ -1,5 +1,6 @@
 # activation function
 import tensorflow as tf
+import numpy as np
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
@@ -11,3 +12,26 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     else:
         outputs = activation_function(Wx_plus_b)
     return outputs
+
+
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+predition = add_layer(l1, 10, 1, activation_function=None)
+loss = tf.reduce_mean(
+    tf.reduce_sum(tf.square(ys - predition), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(1000):
+        sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+        if i % 50 == 0:
+            # to see the step improvement
+            print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
